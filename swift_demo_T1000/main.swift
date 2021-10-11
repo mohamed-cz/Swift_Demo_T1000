3592018bba29579e33b04c10e326444ac47a40da
mac <mac@macs-MacBook-Pro.local>
2021-10-11 11:11:27 +0300

    this is Moh work

--- swift_demo_T1000/main.swift
+++ swift_demo_T1000/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
-
+print("enter your marks in double")
+let mark: Double = Utils.readDouble()
+print("enter full mark")
+let fullMark:Int = Utils.readInt()
+let Pct = mark / Double(fullMark) * 100
+print("Pct = " , round(Pct*100)/100 , "%")
